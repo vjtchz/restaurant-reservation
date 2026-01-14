27fa25160dbf2502e3c8c231d35ed6312a28c2e7
Vojtěch Žídek <vojtech.zidek@hidesign.cz>
2026-01-14 17:36:16 +0100

    feat: add reservation factory

---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'user_id' => User::factory(),
+      'date' => $this->faker->date(),
+      'time_from' => '18:00',
+      'time_to' => '20:00',
+      'guests' => 2,
+    ];
+  }
+}
