b2f7a4bd14937245fed230810d604804f855ee93
Vojtěch Žídek <vojtech.zidek@hidesign.cz>
2026-01-19 12:26:20 +0100

    feat: FlashMessage commponent

--- resources/js/components/FlashMessage.vue
+++ resources/js/components/FlashMessage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
 import { usePage } from '@inertiajs/vue3';
 import { X } from 'lucide-vue-next';
+import { computed, ref, watch } from 'vue';
 
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
