f45b2bf4cd4892149f30325c1a92bc2395085430
Vojtěch Žídek <vojtech.zidek@hidesign.cz>
2026-01-19 15:07:20 +0100

    fix: remove middleware auth from api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,5 @@
 use App\Http\Controllers\Api\ReservationAvailabilityController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
-  Route::get('reservations/availability', ReservationAvailabilityController::class)
-    ->name('reservations.availability');
-});
+Route::get('reservations/availability', ReservationAvailabilityController::class)
+  ->name('reservations.availability');
